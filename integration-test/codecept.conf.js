3e7331fe3f21d6561678d44510cfaa08e4fb68df
Matías Schröder <matischroder99@gmail.com>
2021-11-23 11:22:55 -0300

    integration test configuration

--- integration-test/codecept.conf.js
+++ integration-test/codecept.conf.js
@@ -13,5 +13,23 @@ exports.config = {
       show: true,
       browser: 'chromium'
     }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'integration',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
   }
 }
