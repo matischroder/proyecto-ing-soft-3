90f12ce3b4a1644aabcaacacd05be7e2f2da49b0
Matías Schröder <matischroder99@gmail.com>
2021-11-23 09:46:29 -0300

    Integration test code done it

--- integration-test/codecept.conf.js
+++ integration-test/codecept.conf.js
@@ -8,36 +8,10 @@ exports.config = {
   tests: 'y/*_test.js',
   output: 'y',
   helpers: {
-    WebDriver: {
-      url: 'https://proyecto-ing-soft-3-vote.herokuapp.com/',
-      browser: 'chrome',
-      restart: false,
-      windowSize: '1920x1680',
-      desiredCapabilities: {
-        chromeOptions: {
-          args: [ /*"--headless",*/ "--disable-gpu", "--no-sandbox"]
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'integration-test',
-  plugins: {
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true
-    },
-    tryTo: {
-      enabled: true
-    },
-    screenshotOnFail: {
-      enabled: true
-    },
-    wdio: {
-      enabled: true,
-      services: ['selenium-standalone']
+    Playwright: {
+      url: "http://localhost",
+      show: true,
+      browser: 'chromium'
     }
   }
 }
---
+++ integration-test/y/integration_test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+
+Feature('Votes');
+
+Scenario('Number of votes reflected in results', async ({ I }) => {
+    await I.executeScript(() => {
+        window.open('https://proyecto-ing-soft-3-result.herokuapp.com/','new window')
+    })
+    let votes = await I.grabTextFromAll('#result > span');
+
+    await I.executeScript(() => {
+        window.open('https://proyecto-ing-soft-3-vote.herokuapp.com/', 'new window')
+    })
+    I.amOnPage('https://proyecto-ing-soft-3-vote.herokuapp.com/')
+    I.see('Argentina vs Brazil!')
+    I.seeElement('button.a')
+    I.seeElement('button.b')
+    I.click('#a')
+    I.see('(Tip: you can change your vote)')
+
+    
+    await I.executeScript(() => {
+        window.open('https://proyecto-ing-soft-3-result.herokuapp.com/', 'new window')
+    })
+    let votesVerification = await I.grabTextFromAll('#result > span')
+
+
+    assert.equal(votes[0],votesVerification[0])
+});
+
