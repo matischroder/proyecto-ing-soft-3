7c6c7da6e03cce74cae6c46b6a6baec5e330d2c6
Matías Schröder <matischroder99@gmail.com>
2021-11-23 19:01:48 -0300

    Integration test finished

--- integration-test/integration_test.js
+++ integration-test/integration_test.js
@@ -2,11 +2,12 @@ const assert = require('assert');
 
 Feature('Votes');
 
-Scenario('Number of votes reflected in results', async ({ I }) => {
-    await I.executeScript(() => {
-        window.open('https://proyecto-ing-soft-3-result.herokuapp.com/', 'new window')
-    })
-    let votes = await I.grabTextFromAll('#result > span');
+Scenario('Votes reflected in results', async ({ I }) => {
+    I.amOnPage('https://proyecto-ing-soft-3-result.herokuapp.com/')
+    I.see('ARG')
+    I.see('BRZ')
+    let votes1 = await I.grabTextFrom('#result > span')
+    console.log("We actually have " + votes1)
 
     await I.executeScript(() => {
         window.open('https://proyecto-ing-soft-3-vote.herokuapp.com/', 'new window')
@@ -18,14 +19,23 @@ Scenario('Number of votes reflected in results', async ({ I }) => {
     I.click('#a')
     I.see('(Tip: you can change your vote)')
 
+    I.switchTo();
+    I.amOnPage('https://proyecto-ing-soft-3-result.herokuapp.com/')
+    let votes2 = await I.grabTextFrom('#result > span')
+    console.log("Now we have " + votes2)
 
-    await I.executeScript(() => {
-        window.open('https://proyecto-ing-soft-3-result.herokuapp.com/', 'new window')
-    })
-    let votesVerification = await I.grabTextFromAll('#result > span')
+    I.switchToNextTab();
+    I.clearCookie();
+    I.click('#b');
 
+    I.switchTo();
+    I.amOnPage('https://proyecto-ing-soft-3-result.herokuapp.com/')
+    let votes3 = await I.grabTextFrom('#result > span')
+    console.log("Finally we have " + votes1)
 
-    assert.equal(votes[0], votesVerification[0])
+    votes1 = parseInt(votes1.replace(/\D/g, ""));
+    votes2 = parseInt(votes2.replace(/\D/g, ""));
+    votes3 = parseInt(votes3.replace(/\D/g, ""));
 
+    assert.equal(votes1,votes2-1,votes2-2)
 });
-
